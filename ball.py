fa28799c33882e23cbe8cf1834617f1cb94ecc23
neoneopuooui <trydjfr@gmail.com>
2025-06-29 21:30:58 +0200

    Added Trail

--- ball.py
+++ ball.py
@@ -5,19 +5,35 @@ from ground import *
 class Ball:
     fallSpeed = 0.5
     Balls = []
+    Ghosts = []
+
     def new(self,x,y,radius):
         self.Balls.append([x,y,radius,(random.randint(0,255),random.randint(0,255),random.randint(0,255)),0,20])
+        print("Creating Ball", len(self.Balls))
 
-    def update(self,screen):
+    def update(self,screen, trail):
+        for obj in self.Ghosts:
+            obj[3]-=15
+            if obj[3]<0:
+                obj[3]=0
+            pygame.draw.circle(trail, (255,255,255,obj[3]), (obj[0],obj[1]), obj[2])
+            if obj[3]==0:
+                self.Ghosts.remove(obj)
+
+
         for obj in self.Balls:
             pygame.draw.circle(screen, obj[3], (obj[0],obj[1]), obj[2])
             if obj[1]+obj[2] >= Ground.Y:
                 obj[4]=0
                 obj[4]-=obj[5]
                 obj[5]-=0.5
+
             else:
                 obj[4]+=self.fallSpeed
             obj[1]+=obj[4]
 
-            if obj[5]<0:
+            self.Ghosts.append([obj[0],obj[1],obj[2],255])
+
+            if obj[5]<5:
                 self.Balls.remove(obj)
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+
+from pygame.time import Clock
+
 from ball import *
 from ground import *
 import threading
@@ -9,6 +12,7 @@ import threading
 width, height = 1280,720
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Jumping Balls")
+trail_surf = pygame.Surface((width, height), pygame.SRCALPHA)
 
 running = True
 
@@ -16,12 +20,18 @@ running = True
 
 clock = pygame.time.Clock()
 
+pygame.font.init()
+
+font = pygame.font.SysFont("Comic Sans MS", 30)
+
 ballTimer = 1
 
+maxBalls = 60
+
 while running:
-
+    clock.tick(60)
     screen.fill((0,0,0))
-
+    screen.blit(trail_surf, (0,0))
 
 
     for event in pygame.event.get():
@@ -30,12 +40,18 @@ while running:
             exit(0)
 
     ballTimer+=clock.get_time()/1000
-    if ballTimer>=0.5:
-        Ball.new(Ball,random.randint(15,1265),150,20)
+    if ballTimer>=random.randint(0,4):
+        if len(Ball.Balls)+1<=maxBalls:
+            Ball.new(Ball,random.randint(15,1265),150,random.randint(1,50))
         ballTimer=0
 
-    Ground.update(Ground, screen, width)
-    Ball.update(Ball, screen)
 
-    clock.tick(60)
+    maxBalls = clock.get_fps()
+    fps_text = font.render(str(clock.get_fps()), True, (255,255,255))
+    screen.blit(fps_text, (0,0))
+
+    Ground.update(Ground, screen, width)
+    Ball.update(Ball, screen, trail_surf)
+
+
     pygame.display.update()
